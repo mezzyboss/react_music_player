ffb57137bd13ee6e055127cbcdf5fcf8f630fc9e
Chimemezue Ezeani <mezzymana@gmail.com>
2024-12-11 21:19:03 -0600

    updated vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,22 @@
-/* eslint-disable quotes */
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
+  ],
+  build: {
+    rollupOptions: {
+      input: 'src/index.html',
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  }
 });
